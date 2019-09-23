af3c204990419d444f927674a1616f335a871286
Josue <josgar_14@hotmail.com>
2019-09-23 01:07:40 -0400

    Reactive Form App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'ReactiveForm';
+	title = 'ReactiveForm';
+	myFormModel: FormGroup;
+
+	constructor() {
+		this.myFormModel = new FormGroup({
+			username: new FormControl(),
+			ssn: new FormControl(),
+			passwordsGroup: new FormGroup({
+				password: new FormControl(),
+				pconfirm: new FormControl()
+			})
+		});
+	}
+
+	onSubmit() {
+		console.log(this.myFormModel.value);
+	}
 }
